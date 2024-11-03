86d013c33138b8437f34e27f5f3c7bcf45d3d225
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2024-11-03 09:48:47 -0500

    animaciones del personaje principal
    Animaciones de movimiento para el personaje principal

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public GameObject player;
     public float velocidadMovimiento=10;
     public Rigidbody2D rb;
+    public Animator animator;
     void Start()
     {
         
@@ -29,8 +30,34 @@ public class PlayerMovement : MonoBehaviour
     {
         float movimientoH = Input.GetAxisRaw("Horizontal");
         float movimientoV = Input.GetAxisRaw("Vertical");
+         Vector3 direccion= new Vector3(movimientoH, movimientoV);
+        AnimateMovement(direccion);
+
         rb.velocity = new Vector2(movimientoH * velocidadMovimiento, rb.velocity.y);
         rb.velocity = new Vector2(rb.velocity.x, movimientoV * velocidadMovimiento);
+ 
+    }
+
+    void AnimateMovement(Vector3 direccion)
+    {
+        if (animator != null)
+        {
+            if (direccion.magnitude > 0)
+            {
+                animator.SetBool("Moving", true);
+                animator.SetFloat("Horizontal", direccion.x);
+                animator.SetFloat("Vertical", direccion.y);
+                animator.SetFloat("LastHorizontal", direccion.x);
+                animator.SetFloat("LastVertical", direccion.y);
+            }
+            else
+            {
+                animator.SetBool("Moving", false);
+
+            }
+        }
+
+
     }
 
 
